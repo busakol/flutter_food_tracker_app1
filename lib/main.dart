a0364f4ddb50ad33f713c5db77ac268086dda51c
Busakol <popolalee11@gmail.com>
2026-04-28 14:09:42 +0700

    commit 4 มั้ง อัพเดทหน้า service / main / add หน้า run หายแดงแล้ว แต่ยังดึงข้อมูลขึ้นมาโชว์ไม่ได้

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task_app/views/splash_screen_ui.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  //-------------------การกำหนดค่าการติดต่อกับ project บน Supervase ที่จพทำงานด้วย------------------------
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://buygusvsmvajhphowlmh.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eWd1c3ZzbXZhamhwaG93bG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMDkyMzMsImV4cCI6MjA5MDc4NTIzM30.dV9IluBbG4Efqvt9WE-G1_zPHf3lPwLJ6W39vvkf5ps',
+  );
+
+  //-------------------------------------------------------------------------------------------------
   runApp(
-    FlutterFoodTrackerApp(),
+    FlutterTaskApp(),
   );
 }
 
-class FlutterFoodTrackerApp extends StatelessWidget {
-  const FlutterFoodTrackerApp({super.key});
+class FlutterTaskApp extends StatefulWidget {
+  const FlutterTaskApp({super.key});
 
+  @override
+  State<FlutterTaskApp> createState() => _FlutterTaskAppState();
+}
+
+class _FlutterTaskAppState extends State<FlutterTaskApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreenUi(),
       theme: ThemeData(
-        textTheme: GoogleFonts.promptTextTheme(),
+        textTheme: GoogleFonts.promptTextTheme(Theme.of(context).textTheme),
       ),
     );
   }
--- lib/views/show_all_food_tracker_ui.dart
+++ lib/views/show_all_food_tracker_ui.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: sort_child_properties_last, unused_import
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_task_app/models/task.dart';
+import 'package:flutter_task_app/services/supabase_service.dart';
 import 'package:flutter_task_app/views/add_food_tracker_ui.dart';
-import 'package:flutter_task_app/views/update_delete_food_tracker_ui.dart';
 
 class ShowAllFoodTrackerUi extends StatefulWidget {
   const ShowAllFoodTrackerUi({super.key});
@@ -11,6 +14,31 @@ class ShowAllFoodTrackerUi extends StatefulWidget {
 }
 
 class _ShowAllFoodTrackerUiState extends State<ShowAllFoodTrackerUi> {
+  // สร้าง instance/object/ตัวแทนของ SupabaseService เพื่อเรียกใช้ฟังก์ชันการทำงานต่างๆ กับ Supabase
+  final service = SupabaseService();
+
+  // สร้างตัวแปรเพื่อเก็บข้อมูลที่จะดึงมาจาก Supabase เพื่อไปแสดงใน ListView
+  List<Task> tasks = [];
+
+  // สร้างเมธอดเพื่อดึงข้อมูล แล้วเอาข้อมูลที่ดึงมากำหนดให้กับตัวแปร
+  void loadTasks() async {
+    //ดึงข้อมูลจาก Supabase
+    final data = await service.getAllTask();
+
+    //เอาข้อมูลที่ดึงมา กำหนดให้กับตัวแปร
+    setState(() {
+      tasks = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    //เรียกใช้เมธอด loadTasks() ให้ทำงานตอนหน้าจอถูกเปิด
+    loadTasks();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,6 +71,68 @@ class _ShowAllFoodTrackerUiState extends State<ShowAllFoodTrackerUi> {
         //ตำแหน่งของ FloatingActionButton
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // ส่วนของ Body
+      body: Center(
+        child: Column(
+          children: [
+            //ส่วนแสดงรูปโลโก้
+            SizedBox(height: 50),
+            Image.asset(
+              'assets/icon/icon.png',
+              width: 120,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 50),
+            //ส่วนของ ListView แสดงรายการข้อมูลงานที่ดึงมาจาก Supabase
+            Expanded(
+              child: ListView.builder(
+                // จำนวนรายการใน ListView
+                itemCount: tasks.length,
+                // สร้างหน้าตาของแต่ละรายการใน ListView
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      left: 40,
+                      right: 40,
+                      top: 5,
+                      bottom: 5,
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        'กิน: ${tasks[index].foodName}',
+                      ),
+                      subtitle: Text(
+                        // ternary operator ____ ? ____ : ______
+                        'มื้อ: ${tasks[index].foodMeal}',
+                      ),
+                      trailing: Icon(
+                        Icons.info,
+                        color: Colors.green,
+                      ),
+                      leading: tasks[index].foodPrice == ''
+                          ? Image.asset(
+                              'assets/icon/icon.png',
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                            )
+                          : Image.network(
+                              tasks[index].foodImagesUrl!,
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                            ),
+                      tileColor:
+                          index % 2 == 0 ? Colors.blue[200] : Colors.grey,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
